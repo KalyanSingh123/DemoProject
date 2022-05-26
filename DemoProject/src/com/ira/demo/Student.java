e6e99b62718836ec2dc883d615a938e7c0f16f6c
kalyan <kalyan@kalyan-ThinkPad>
2022-05-26 16:44:40 +0530

    update Student class

--- DemoProject/src/com/ira/demo/Student.java
+++ DemoProject/src/com/ira/demo/Student.java
@@ -1,5 +1,25 @@
 package com.ira.demo;
 
 public class Student {
-
+	private Long studentId;
+	private String studentName;
+	private String studentRollNumber;
+	public Long getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentRollNumber() {
+		return studentRollNumber;
+	}
+	public void setStudentRollNumber(String studentRollNumber) {
+		this.studentRollNumber = studentRollNumber;
+	}
 }
