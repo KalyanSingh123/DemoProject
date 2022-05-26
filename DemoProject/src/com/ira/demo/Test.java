5873b4dd9e4456d9572a883a3a10de4084f4f228
kalyan <kalyan@kalyan-ThinkPad>
2022-05-26 17:05:15 +0530

    changes in Test.java file

--- DemoProject/src/com/ira/demo/Test.java
+++ DemoProject/src/com/ira/demo/Test.java
@@ -6,6 +6,8 @@ public class Test {
 		s1.setStudentId(1l);
 		s1.setStudentName("Rajat");
 		s1.setStudentRollNumber("1");
+		System.out.println("Student Name : "+s1.getStudentName());
 		System.out.println("Hey, It is only for testing!!");
+		
 	}
 }
