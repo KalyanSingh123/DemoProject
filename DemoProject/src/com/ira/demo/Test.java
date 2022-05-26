cc30b323e06324d41d3eb6860a9639d39a448c14
kalyan <kalyan@kalyan-ThinkPad>
2022-05-26 16:55:36 +0530

    changes in Test.java

--- DemoProject/src/com/ira/demo/Test.java
+++ DemoProject/src/com/ira/demo/Test.java
@@ -2,6 +2,10 @@ package com.ira.demo;
 
 public class Test {
 	public static void main(String[] args) {
+		Student s1 = new Student();
+		s1.setStudentId(1l);
+		s1.setStudentName("Rajat");
+		s1.setStudentRollNumber("1");
 		System.out.println("Hey, It is only for testing!!");
 	}
 }
